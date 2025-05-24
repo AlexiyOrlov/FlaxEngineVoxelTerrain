6eb5b05b4b5cb21353ac2d1a001f558242bcb110
AlexiyOrlov <alexiy.ov@gmail.com>
2025-05-24 22:36:21 +0300

    Second

---
+++ Source/Game/ChunkPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game.Game
+{
+    public struct ChunkPart
+    {
+        public List<Float3> vertices;
+        public List<int> triangles ;
+        public List<Vector2> uvs ;
+        // public  Voxel[,,] voxels;
+        // public VoxelType voxelType;
+
+        public ChunkPart()
+        {
+            // voxelType = type;
+            vertices = new List<Float3>();
+            triangles = new List<int>();
+            uvs = new List<Vector2>();
+            // voxels = new Voxel[World.Instance.chunkSize, World.Instance.chunkSize, World.Instance.chunkSize];
+        }
+    }
+}
---
+++ Source/Game/CubePlacer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using FlaxEngine;
+using FlaxEngine.Utilities;
+
+namespace Game;
+
+public class CubePlacer : Script
+{
+    private Model _model;
+    public MaterialBase Material;
+    
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        _model = Content.CreateVirtualAsset<Model>();
+        _model.SetupLODs([1]);
+        UpdateMesh(_model.LODs[0].Meshes[0]);
+
+        // Create or reuse child model
+        var childModel = Actor.GetOrAddChild<StaticModel>();
+        childModel.Model = _model;
+        childModel.LocalScale = new Float3(100);
+        childModel.SetMaterial(0, Material);
+    }
+    
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        // Here you can add code that needs to be called every frame
+    }
+
+    public override void OnDestroy()
+    {
+        Destroy(_model);
+    }
+
+    private void UpdateMesh(Mesh mesh)
+    {
+        var vertices = new List<Float3>();
+        var triangles =new List<int>();
+        var uvs = new List<Float2>();
+        for (int i = 0; i < 6; i++)
+        {
+            AddFaceData(0,0,0,i,vertices,triangles,uvs);
+        }
+        List<Float3> normals = new ();
+        mesh.UpdateMesh(vertices.ToArray(), triangles.ToArray(),null,null,uvs.ToArray());
+    }
+    
+    private void AddFaceData(int x, int y, int z, int faceIndex,List<Float3> vertices,List<int> triangles,List<Float2> uvs)
+    {
+        // Based on faceIndex, determine vertices and triangles
+        // Add vertices and triangles for the visible face
+        // Calculate and add corresponding UVs
+        // float3 pos=new float3(x,y,z);
+        // ChunkPart chunkPart=positionToChunkParts[pos];
+        // x+=(int)position.x;
+        // y+=(int)position.y;
+        // z+=(int)position.z;
+        if (faceIndex == 0) // Top Face
+        {
+            vertices.Add(new Float3(x,     y + 1, z    ));
+            vertices.Add(new Vector3(x,     y + 1, z + 1)); 
+            vertices.Add(new Vector3(x + 1, y + 1, z + 1));
+            vertices.Add(new Vector3(x + 1, y + 1, z    )); 
+            uvs.Add(new Vector2(0, 0));
+            uvs.Add(new Vector2(1, 0));
+            uvs.Add(new Vector2(1, 1));
+            uvs.Add(new Vector2(0, 1));
+        }
+
+        if (faceIndex == 1) // Bottom Face
+        {
+            vertices.Add(new Vector3(x,     y, z    ));
+            vertices.Add(new Vector3(x + 1, y, z    )); 
+            vertices.Add(new Vector3(x + 1, y, z + 1));
+            vertices.Add(new Vector3(x,     y, z + 1)); 
+            uvs.Add(new Vector2(0, 0));
+            uvs.Add(new Vector2(0, 1));
+            uvs.Add(new Vector2(1, 1));
+            uvs.Add(new Vector2(1, 0));
+        }
+
+        if (faceIndex == 2) // Left Face
+        {
+            vertices.Add(new Vector3(x, y,     z    ));
+            vertices.Add(new Vector3(x, y,     z + 1));
+            vertices.Add(new Vector3(x, y + 1, z + 1));
+            vertices.Add(new Vector3(x, y + 1, z    ));
+            uvs.Add(new Vector2(0, 0));
+            uvs.Add(new Vector2(0, 0));
+            uvs.Add(new Vector2(0, 1));
+            uvs.Add(new Vector2(0, 1));
+        }
+
+        if (faceIndex == 3) // Right Face
+        {
+            vertices.Add(new Vector3(x + 1, y,     z + 1));
+            vertices.Add(new Vector3(x + 1, y,     z    ));
+            vertices.Add(new Vector3(x + 1, y + 1, z    ));
+            vertices.Add(new Vector3(x + 1, y + 1, z + 1));
+            uvs.Add(new Vector2(1, 0));
+            uvs.Add(new Vector2(1, 1));
+            uvs.Add(new Vector2(1, 1));
+            uvs.Add(new Vector2(1, 0));
+        }
+
+        if (faceIndex == 4) // Front Face
+        {
+            vertices.Add(new Vector3(x,     y,     z + 1));
+            vertices.Add(new Vector3(x + 1, y,     z + 1));
+            vertices.Add(new Vector3(x + 1, y + 1, z + 1));
+            vertices.Add(new Vector3(x,     y + 1, z + 1));
+            uvs.Add(new Vector2(0, 1));
+            uvs.Add(new Vector2(0, 1));
+            uvs.Add(new Vector2(1, 1));
+            uvs.Add(new Vector2(1, 1));
+        }
+
+        if (faceIndex == 5) // Back Face
+        {
+            vertices.Add(new Vector3(x + 1, y,     z    ));
+            vertices.Add(new Vector3(x,     y,     z    ));
+            vertices.Add(new Vector3(x,     y + 1, z    ));
+            vertices.Add(new Vector3(x + 1, y + 1, z    ));
+            uvs.Add(new Vector2(0, 0));
+            uvs.Add(new Vector2(1, 0));
+            uvs.Add(new Vector2(1, 0));
+            uvs.Add(new Vector2(0, 0));
+        
+        }
+            
+        int vertCount = vertices.Count;
+
+        // First triangle
+        triangles.Add(vertCount - 4);
+        triangles.Add(vertCount - 3);
+        triangles.Add(vertCount - 2);
+        
+        // Second triangle
+        triangles.Add(vertCount - 4);
+        triangles.Add(vertCount - 2);
+        triangles.Add(vertCount - 1);
+    }
+
+}
